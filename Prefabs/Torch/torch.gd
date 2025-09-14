b9128b88c3ab60591083d37cd3b7953e6c492e00
guihPprt <guilherme.oliveira03032008@gmail.com>
2025-09-14 16:11:56 -0400

    a01

---
+++ Prefabs/Torch/torch.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$Label.position.y += -sin(Time.get_ticks_msec()*.010)*.2
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		$Label.visible = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		$Label.visible = false
