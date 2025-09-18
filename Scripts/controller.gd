77ea6e87b2a34a272de2047e94d6d2cfdc3dc860
guihPprt <guilherme.oliveira03032008@gmail.com>
2025-09-18 00:32:57 +0000

    Port mobile, Web, Windows e Linux

--- Scripts/controller.gd
+++ Scripts/controller.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+@onready var mobile_hud
+
+func _ready() -> void:
+	mobile_hud = get_tree().current_scene.get_node("MobileHUD")
+	
+	if OS.get_name() != "Android":
+		mobile_hud.queue_free()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("fs"):
